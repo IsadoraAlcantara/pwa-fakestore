1c7723d0c6a8916c02fbdcff7ca19a89b313caa1
IsadoraAlcantara <isadoraalca405@gmail.com>
2024-02-27 15:14:14 -0300

    alterando css

--- src/components/ListagemProdutos.vue
+++ src/components/ListagemProdutos.vue
@@ -1,24 +1,28 @@
 <script setup>
-import { ref, onMounted } from 'vue';
-import axios from 'axios';
-const produtos = ref([]);
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+const produtos = ref([])
 
 onMounted(async () => {
-  const response = await axios.get('https://fakestoreapi.com/products');
-  produtos.value = response.data;
-});
+  const response = await axios.get('https://fakestoreapi.com/products')
+  produtos.value = response.data
+})
 
-const formatPrice = (price) => `R$ ${price.toFixed(2).replace('.', ',')}`;
+const formatPrice = (price) => `R$ ${price.toFixed(2).replace('.', ',')}`
 </script>
 <template>
   <div>
-    <h1>Produtos</h1>
+    <h1 class="title">Produtos</h1>
     <div class="container">
       <div class="card" v-for="produto in produtos" :key="produto.id">
-        <h1 class="card--title">{{ produto.title }}</h1>
-        <p>{{ produto.description }}</p>
-        <p>{{ formatPrice(produto.price) }}</p>
-        <img class="card--avatar" :src="produto.image" :alt="produto.title" />
+        <div>
+          <img class="card--avatar" :src="produto.image" :alt="produto.title" />
+        </div>
+        <div class="text">
+          <h1 class="card--title">{{ produto.title }}</h1>
+          <p class="description">{{ produto.description }}</p>
+          <p class="price">{{ formatPrice(produto.price) }}</p>
+        </div>
       </div>
     </div>
   </div>
@@ -39,6 +43,21 @@ const formatPrice = (price) => `R$ ${price.toFixed(2).replace('.', ',')}`;
   }
 }
 
+.text {
+  width: 50%;
+}
+
+.title {
+  margin: 2vw 0 0 4vw;
+}
+
+.description {
+  color: #565D73;
+}
+
+.price {
+  margin-top: 1vw;
+}
 .container {
   display: flex;
   flex-wrap: wrap;
@@ -49,23 +68,25 @@ const formatPrice = (price) => `R$ ${price.toFixed(2).replace('.', ',')}`;
   padding: 1rem 0;
 }
 .card {
+  padding: 2vw;
   display: flex;
-  align-items: center;
   justify-content: space-between;
-  flex-direction: column;
-  width: 15rem;
+  width: 40rem;
   height: 25rem;
   background: #fff;
-  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
+  box-shadow:
+    0 10px 20px rgba(0, 0, 0, 0.19),
+    0 6px 6px rgba(0, 0, 0, 0.23);
   border-radius: 10px;
   margin: auto;
   overflow: hidden;
 }
 .card--avatar {
-  width: 100%;
+  width: 17vw;
   height: 17rem;
   object-fit: cover;
   margin-bottom: 0.5rem;
+  border-radius: 1vw;
 }
 .card--title {
   color: #222;
