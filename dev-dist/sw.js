75b3e9d47502139611aca6779b388571557f93f7
IsadoraAlcantara <isadoraalca405@gmail.com>
2024-03-19 14:53:44 -0300

    removendo arquivos

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.a7q7b70dvk8"
+    "revision": "0.u4g127deiqg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
